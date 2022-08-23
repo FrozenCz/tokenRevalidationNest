d86d4be4945cbfd3c090d06869055be84178de01
FrozenCz <info@milanknop.cz>
2022-08-23 10:17:51 +0200

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from "./jwt.strategy";
+import { JwtStrategy } from './jwt.strategy';
 
 export const jwtConstants = {
   secret: 'myTestkey',
